ff185945583cbe90420032c8f894af2194600848
tanjiina <tanjina1042@gmail.com>
2024-02-18 21:57:07 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// name auto chole ashbe tai use kortesi
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
